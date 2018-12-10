3f51e8750d2b82e4d6073ea86eea0dc5151b1a91
pythonmood <45766102+pythonmood@users.noreply.github.com>
2018-12-10 19:51:50 +0330

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,251 @@
+﻿var data=[{
+
+	text: 'Layers',isfolder:true,
+	state: 'opened',
+		children: [{
+		text: 'Iran',isfolder:true,
+		state: 'opened',
+
+
+
+
+			children: [{
+			text: 'Airport',isfolder:true,
+			state: 'opened',
+			children: [
+			
+				{
+				text: 'البرز',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 37391, text: "البرز_code_1", mtype:"shp", url: "layerdata/airport/Alborz.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'اردبيل',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 69187, text: "اردبيل_code_1", mtype:"shp", url: "layerdata/airport/Ardebil.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'آذربايجان‌غربي',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 92611, text: "آذربايجان‌غربي_code_1", mtype:"shp", url: "layerdata/airport/Azar_Gharbi.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'آذربايجان‌شرقي',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 94675, text: "آذربايجان‌شرقي_code_1", mtype:"shp", url: "layerdata/airport/Azar_Sharghi.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'بوشهر',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 47047, text: "بوشهر_code_1", mtype:"shp", url: "layerdata/airport/Booshehr.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'چهارمحال و بختياري',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 80937, text: "چهارمحال و بختياري_code_1", mtype:"shp", url: "layerdata/airport/Cheharmahal.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'اصفهان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 86583, text: "اصفهان_code_1", mtype:"shp", url: "layerdata/airport/Esfehan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'فارس',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 98033, text: "فارس_code_1", mtype:"shp", url: "layerdata/airport/Fars.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'قزوين',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 81154, text: "قزوين_code_1", mtype:"shp", url: "layerdata/airport/Ghazvin.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'قم',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 52848, text: "قم_code_1", mtype:"shp", url: "layerdata/airport/Ghom.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'گيلان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 33372, text: "گيلان_code_1", mtype:"shp", url: "layerdata/airport/Gilan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'گلستان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 16991, text: "گلستان_code_1", mtype:"shp", url: "layerdata/airport/Golestan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'همدان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 62441, text: "همدان_code_1", mtype:"shp", url: "layerdata/airport/Hamedan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'هرمزگان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 63238, text: "هرمزگان_code_1", mtype:"shp", url: "layerdata/airport/Hormozgan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'ايلام',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 65854, text: "ايلام_code_1", mtype:"shp", url: "layerdata/airport/Ilam.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'کرمان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 46708, text: "کرمان_code_1", mtype:"shp", url: "layerdata/airport/Kerman.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'كرمانشاه',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 17683, text: "كرمانشاه_code_1", mtype:"shp", url: "layerdata/airport/Kermanshah.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'خوزستان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 55361, text: "خوزستان_code_1", mtype:"shp", url: "layerdata/airport/Khoozestan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'خراسان‌جنوبي',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 77112, text: "خراسان‌جنوبي_code_1", mtype:"shp", url: "layerdata/airport/Khorasan_Jonoobi.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'خراسان‌رضوي',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 81602, text: "خراسان‌رضوي_code_1", mtype:"shp", url: "layerdata/airport/Khorasan_Razavi.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'خراسان‌شمالي',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 75334, text: "خراسان‌شمالي_code_1", mtype:"shp", url: "layerdata/airport/Khorasan_Shomali.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'كهكيلويه و بويراحمد',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 56324, text: "كهكيلويه و بويراحمد_code_1", mtype:"shp", url: "layerdata/airport/Kohkilooye_Boyerahmad.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'كردستان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 49381, text: "كردستان_code_1", mtype:"shp", url: "layerdata/airport/Kordestan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'لرستان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 45179, text: "لرستان_code_1", mtype:"shp", url: "layerdata/airport/Lorestan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'مركزي',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 25031, text: "مركزي_code_1", mtype:"shp", url: "layerdata/airport/Markazi.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'مازندران',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 17937, text: "مازندران_code_1", mtype:"shp", url: "layerdata/airport/Mazandaran.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'سمنان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 81699, text: "سمنان_code_1", mtype:"shp", url: "layerdata/airport/Semnan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'سيستان و بلوچستان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 81102, text: "سيستان و بلوچستان_code_1", mtype:"shp", url: "layerdata/airport/Sistan_Baloochestan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'تهران',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 56543, text: "تهران_code_1", mtype:"shp", url: "layerdata/airport/Tehran.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'يزد',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 32346, text: "يزد_code_1", mtype:"shp", url: "layerdata/airport/Yazd.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+				{
+				text: 'زنجان',isfolder:true,
+				state: 'closed',
+				children: [{iconCls:"icon-Point", ticon:"", lid : 34362, text: "زنجان_code_1", mtype:"shp", url: "layerdata/airport/Zanjan.zip",info:"<p style='text-align:right'>اين لايه به صورت دستي و بر اساس اطلاعات گوگل ارث تهيه شده و ممکن است تمامي اطلاعات را شامل نشود</p>"},]
+				},
+
+			]
+			}]
+
+
+
+
+
+
+
+
+
+
+
+		}]
+
+
+
+	}
+	,
+	{
+		iconCls:"icon-Basemap",
+		text: 'Basemaps',isfolder:true,
+		state: 'closed',
+		children: [{
+			iconCls:"icon-Basemap",checked:true, ticon:"", text: 'OpenStreetMap HOT', mtype:"Basemaps", mapname: "OpenStreetMap_HOT"
+			},
+			{
+			iconCls:"icon-Basemap",checked:false, ticon:"", text: 'OpenTopoMap', mtype:"Basemaps", mapname: "OpenTopoMap"
+			},
+			{
+			iconCls:"icon-Basemap",checked:false, ticon:"", text: 'OpenStreetMap_Mapnik', mtype:"Basemaps", mapname: "OpenStreetMap_Mapnik"
+			},
+			{
+			iconCls:"icon-Basemap",checked:false, ticon:"", text: 'OpenMapSurfer_Roads', mtype:"Basemaps", mapname: "OpenMapSurfer_Roads"
+			},
+			{
+			iconCls:"icon-Basemap",checked:false, ticon:"", text: 'Stamen_Watercolor', mtype:"Basemaps", mapname: "Stamen_Watercolor"
+			},
+			{
+			iconCls:"icon-Basemap",checked:false, ticon:"", text: 'Stamen_TerrainBackground', mtype:"Basemaps", mapname: "Stamen_TerrainBackground"
+			},
+			{
+			iconCls:"icon-Basemap",checked:false, ticon:"", text: 'Esri_WorldImagery', mtype:"Basemaps", mapname: "Esri_WorldImagery"
+			}
+			]
+
+	}]
+
+
